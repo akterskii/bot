8c6fe84e163b3f676c619379003ae038dcb94227
Andrei Akterskii <aktersky@gmail.com>
2020-03-26 01:19:57 -0700

    3rd action fixed

--- main_logic/common/mappings.py
+++ main_logic/common/mappings.py
@@ -2,6 +2,6 @@ from main_logic.state_handling.quest_states import Actions
 
 actions_to_comands = {
     #Actions.LIST_ALL_QUESTS: 'list_quests',
-    Actions.EDIT_QUESTS: 'edit_quests',
+    #Actions.EDIT_QUESTS: 'edit_quests',
     Actions.LEVEL_UP: 'level_up'
 }
